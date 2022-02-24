1e3a913f43f4ceb2b9c8b078c47af0d4d68c1ecb
Tommy Bidne <tbidne@protonmail.com>
2022-02-25 11:52:54 +1300

    Switch to haskell.nix for future x-compilation support

--- flake.nix
+++ flake.nix
@@ -3,46 +3,52 @@
   inputs = {
     algebra-simple-src.url = "github:tbidne/algebra-simple/main";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    # See https://discourse.nixos.org/t/nix-2-6-0-released/17324/8
+    haskellNix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.follows = "haskellNix/nixpkgs-2111";
     refined-extras-src.url = "github:tbidne/refined-extras/main";
   };
   outputs =
     { algebra-simple-src
     , flake-utils
+    , haskellNix
     , nixpkgs
     , refined-extras-src
     , self
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
-      compilerVersion = "ghc8107";
-      compiler = pkgs.haskell.packages."${compilerVersion}";
-      mkPkg = returnShellEnv:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "shell-run";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
-              cabal-install
-              cabal-plan
-              haskell-language-server
-              hlint
-              ghcid
-              ormolu
-            ]);
-          overrides = final: prev: with pkgs.haskellPackages; {
-            algebra-simple =
-              final.callCabal2nix "algebra-simple" algebra-simple-src { };
-            refined-extras =
-              final.callCabal2nix "refined-extras" refined-extras-src { };
-          };
-        };
-    in
-    {
-      defaultPackage = mkPkg false;
+      overlays = [
+        haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          shellRunProject =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8107";
 
-      devShell = mkPkg true;
+              # This is needed because cabal.project and stack.yaml
+              # both exist in this repo.
+              projectFileName = "cabal.project";
+            };
+
+          algebra-simple = final.cabal2nix "algebra-simple" algebra-simple-src { };
+          refined-extras = final.cabal2nix "refined-extras" refined-extras-src { };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      flake = pkgs.shellRunProject.flake { };
+    in
+    flake // {
+      defaultPackage = flake.packages."shell-run:exe:shell-run";
+
+      devShell = pkgs.shellRunProject.shellFor {
+        tools = {
+          cabal-install = { };
+          haskell-language-server = { };
+          ghcid = { };
+        };
+      };
     });
 }
